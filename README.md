0ec02939d428b6390b2d4c28888442cfc57485be
psychobolt <psychobolt@gmail.com>
2017-02-22 15:50:53 -0800

    WIP post_corpus.py

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # qbotio-resources
+
+## Development
+
+### Git Setup
+
+Clone the git repository: ```$ git clone https://github.com/techcats/qbotio-resources.git```
+
+### Environment Setup
+
+1. Install [python 3.6+](https://www.python.org/)
+2. Install/Upgrade [pip](https://pip.pypa.io/en/stable/installing/)
+3. Install virtualenv: ```$ pip install virtualenv```
+4. Install virtualenvwrapper. Follow and see usage with [Windows](https://pypi.python.org/pypi/virtualenvwrapper-win), [Linux & Mac](https://virtualenvwrapper.readthedocs.io/en/stable/) guides.
+6. Create (```$ mkvirtualenv qbotio-resources```) and work (```$ workon qbotio-resources```) on a local environment using virtualenvwrapper.
+7. Set the default project directory: ```(qbotio-resources) $ setvirtualenvproject $VIRTUAL_ENV <path to cloned repo>``` (Linux & Mac) or use ```setprojectdir <path to cloned repo>``` (Windows)
+8. ```(qbotio) $ deactivate```
+9. ```$ workon qbotio```
+
+You should now be at the root directory of 'qbotio-resources/'. Calling ```$ workon qbotio``` should now automatically direct to the your sources directory.
+
+> A tip for step 3 & 4 (Linux & Mac): Similiar to ```python3```, ```pip3``` is the python 3.x's equivalent. virtualenvwrapper.sh may require the correct python version in your "VIRTUALENVWRAPPER_PYTHON" environment variable.
+
+### Install/Update python libraries
+
+Run ```(qbotio) $ pip install -r requirements.txt```
+
+> To include a new library use ```(qbotio) $ pip install [package]```, and then ```(qbotio) $ pip freeze > requirements.txt```
---
+++ post_corpus.py
@@ -0,0 +1,95 @@
+import os
+import sys
+import re
+import getopt
+import urllib
+import json
+import requests
+import pprint
+
+def print_usage():
+  print(f'Usage: {sys.argv[0]} <url> [-u user] [-p pass] -f <inputfile> --numdelimiter=")"')
+
+if (len(sys.argv) < 4):
+  print_usage()
+  sys.exit(2)
+
+url = sys.argv[1]
+
+try:
+  opts, args = getopt.getopt(sys.argv[2:], 'hu:p:f:', ['help=', 'user=', 'password=', 'file=', 'numdelimiter='])
+except getopt.GetoptError:
+  print_usage()
+  sys.exit(2)
+
+user = None
+password = None
+auth = []
+
+for opt, arg in opts:
+  pass
+  if opt == '-h':
+    print_usage()
+    sys.exit()
+  elif opt in ('-f', '--file'):
+    filepath = arg.strip()
+  elif opt in ('-u', '--user'):
+    user = arg
+    auth.append(user)
+  elif opt in ('-p', '--password'):
+    password = arg
+    auth.append(password)
+  elif opt == '--numdelimiter':
+    numdelimiter = arg
+if (len(auth) < 2):
+  auth = None
+else:
+  auth = (user, password)
+
+inputfile = open(filepath, 'r', encoding='UTF-8')
+line_index = 0
+question = None
+answer = None
+prev_line = None
+answers = []
+for line in inputfile:
+  if (line_index == 0):
+    origin = line.strip().replace(u'\ufeff', '')
+    line_index += 1
+    # print(repr(origin))
+    continue
+  line_content = line.strip()
+  if (question == None) and (line_content != ''):
+    question = line_content
+    question_count = int(re.search(f'^(\d+){numdelimiter}', line_content).group(1))
+    # print(repr(question))
+  elif line_content != '':
+    match = re.search(f'^(\d+){numdelimiter}', line_content)
+    if match:
+      # print(repr(answer))
+      question = line_content
+      question_count = int(match.group(1))
+      answers.append({
+        'value': answer,
+        'origin': origin,
+        'source': os.path.basename(inputfile.name)
+      })
+      answer = None
+      # print(repr(question))
+    if (prev_line == '\n') and (line != '\n'):
+      answer = f'{line_content} '
+    else:
+      answer += f'{line_content} '
+  line_index += 1
+  prev_line = line
+
+pp = pprint.PrettyPrinter(indent=2, )
+pp.pprint(answers)
+
+# count = 0 
+
+# response = requests.post(url, auth=auth, data=data)
+# if (response.status_code == 201):
+#   count += 1
+
+# print(f'Created {count} entries')
